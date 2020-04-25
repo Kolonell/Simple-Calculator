9060b7cc7bc9f63e92fc04aa4594cf524bbad785
Mohamed <kolonkalan@gmail.com>
2020-04-25 07:57:03 +0200

    Added the code
    It's the code of the calculator.

---
+++ calculator.py
@@ -0,0 +1,125 @@
+from tkinter import *
+
+# We are initializing our program
+root = Tk()
+root.title("Simple Calculator")
+
+# All the results will be shown on the entry section
+e = Entry(root, width=35, borderwidth=5)
+e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+
+# In this section we are implementing what the buttons will do if pressed
+
+def button_click(number):
+    # Put on the entry the selected number
+    current = e.get()
+    e.delete(0, END)
+    e.insert(0,str(current) + str(number))
+
+def button_clear():
+    # Clear the entry
+    e.delete(0,END)
+
+# In the operation functions we use global variables to store in memory the number and the type of operation
+def button_add():
+    first_number = e.get()
+    global f_num
+    global operation
+    operation = "add"
+    f_num = float(first_number)
+    e.delete(0, END)
+
+def button_multiply():
+    first_number = e.get()
+    global f_num
+    global operation
+    operation = "multiply"
+    f_num = float(first_number)
+    e.delete(0, END)
+
+def button_divide():
+    first_number = e.get()
+    global f_num
+    global operation
+    f_num = float(first_number)
+    operation = "divide"
+    e.delete(0, END)
+
+def button_substract():
+    first_number = e.get()
+    global f_num
+    global operation
+    operation = "substract"
+    f_num = float(first_number)
+    e.delete(0, END)
+
+# The egality depend on the operation so we specify the behaviour in function of the type of operation
+def button_equal():
+    second_number = e.get()
+    global f_num
+    global operation
+    if operation == "add":
+        f_num += float(second_number)
+        e.delete(0, END)
+        e.insert(0,f_num)
+    elif operation == "multiply":
+        f_num *= float(second_number)
+        e.delete(0, END)
+        e.insert(0,f_num)
+    elif operation == "divide":
+        f_num /= float(second_number)
+        e.delete(0, END)
+        e.insert(0,f_num)
+    elif operation == "substract":
+        f_num -= float(second_number)
+        e.delete(0, END)
+        e.insert(0,f_num)
+
+# These are the buttons for selecting the numbers
+button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
+button_2 = Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2))
+button_3 = Button(root, text="3", padx=40, pady=20, command=lambda: button_click(3))
+button_4 = Button(root, text="4", padx=40, pady=20, command=lambda: button_click(4))
+button_5 = Button(root, text="5", padx=40, pady=20, command=lambda: button_click(5))
+button_6 = Button(root, text="6", padx=40, pady=20, command=lambda: button_click(6))
+button_7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click(7))
+button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
+button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
+button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
+
+# These are our operation buttons
+button_add = Button(root, text="+", padx=40, pady=20, command=button_add)
+button_equal = Button(root, text="=", padx=40, pady=20, command=button_equal)
+button_multiply = Button(root, text="*", padx=40, pady=20, command=button_multiply)
+button_substract = Button(root, text="-", padx=40, pady=20, command=button_substract)
+button_divide = Button(root, text="/", padx=40, pady=20, command=button_divide)
+
+# This is our clear button
+button_clear = Button(root, text="Clear", padx=128, pady=20, command=button_clear)
+
+# We are now implementing the buttons and their location in the program windows
+
+button_1.grid(row=3, column=0)
+button_2.grid(row=3, column=1)
+button_3.grid(row=3, column=2)
+
+button_4.grid(row=2, column=0)
+button_5.grid(row=2, column=1)
+button_6.grid(row=2, column=2)
+
+button_7.grid(row=1, column=0)
+button_8.grid(row=1, column=1)
+button_9.grid(row=1, column=2)
+
+button_0.grid(row=4, column=0)
+button_add.grid(row=4, column=1, columnspan=1, rowspan=1)
+button_equal.grid(row=4, column=2, columnspan=1, rowspan=1)
+
+button_substract.grid(row=5, column=0, columnspan=1, rowspan=1)
+button_multiply.grid(row=5, column=1, columnspan=1, rowspan=1)
+button_divide.grid(row=5, column=2, columnspan=1, rowspan=1)
+
+button_clear.grid(row=6, column=0, columnspan=3)
+
+# The mainloop must be called for the windows to be drawn and events to be processed
+root.mainloop()
